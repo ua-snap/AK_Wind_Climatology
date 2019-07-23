795f741cb8770d882174ff674adc8f90c8bfda43
unknown <kmeanskyle@gmail.com>
2019-07-23 13:04:28 -0800

    change method for determining WRF grid cells for stations

--- WRF/select_stations_output.R
+++ WRF/select_stations_output.R
@@ -49,57 +49,25 @@ stids <- unique(select_stations$stid)
 #------------------------------------------------------------------------------
 
 #-- Station Cells -------------------------------------------------------------
-# u10 and v10 historical directories (from external drive)
-u10_dir <- file.path(era_output_dir, "ERA_u10")
-v10_dir <- file.path(era_output_dir, "ERA_v10")
-# determine where on the WRF grid each station lies via intersecting station
-#   locations with raster
-# open connection to one of the files 
-u10_fname <- "u10_hourly_wrf_ERA-Interim_historical_"
-u10_path <- file.path(u10_dir, paste0(u10_fname, target_years[1], ".nc"))
-u10 <- nc_open(u10_path)
-# grid is based on centroids, so need to determine correct extent for building
-#   corresponding raster
-# extract x and y from u10
-xc <- ncvar_get(u10, varid = "xc")
-yc <- ncvar_get(u10, varid = "yc")
-nx <- length(xc)
-ny <- length(yc)
-# extract time 0 layer of u component
-t0 <- ncvar_get(u10, "u10", count = c(nx, ny, 1))
-# close connection
-nc_close(u10)
-# resolution of grid would be the difference between two centroids
-res <- xc[2] - xc[1]
-# define extent bounds - pad with half of unit grid size 
-bounds <- c(min(xc), max(xc), min(yc), max(yc)) + c(-res, res)/2
-# Projection from WRF
-wrf_crs <- CRS("+units=m +proj=stere +lat_ts=64.0 +lon_0=-152.0 +lat_0=90.0 +x_0=0 +y_0=0 +a=6370000 +b=6370000")
-r <- raster(t0, xmn = bounds[1], xmx = bounds[2],
-            ymn = bounds[3], ymx = bounds[4],
-            crs = wrf_crs)
+# Use Ancilliary data to determine grid cells for stations
+anc <- nc_open(file.path(datadir, "geo_em.d01.nc"))
+xlat_m <- ncvar_get(anc, "XLAT_M")
+xlon_m <- ncvar_get(anc, "XLONG_M")
+nc_close(anc)
+coords <- cbind(select_stations$lat, select_stations$lon)
+wrf_coords <- array(c(xlat_m, xlon_m), dim = c(262, 262, 2))
 
-# CRS for station coordinates
-crs1 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs+ towgs84=0,0,0")
-# select coords and transform
-stations_sp <- select_stations %>% 
-  dplyr::select(lon, lat) %>% 
-  SpatialPoints(proj4string = crs1) %>%
-  spTransform(wrf_crs)
-# interset points with raster
-u10_select <- raster::extract(r, stations_sp, method = "simple")
-# can try to match extracted wind components
-#   not useful if there are duplicate values
-anyDuplicated(t0)
-# cool, we can match stations based on model output
-f1 <- function(x, set){which(set == x, arr.ind = TRUE)}
-cell_ind <- sapply(u10_select, f1, t0)
-# save these cell indices with stids
-stid_wrf_ind <- data.frame(cbind(stids, t(cell_ind))) %>%
-  rename(stid = stids, wrf_i = V2, wrf_j = V3) %>%
-  mutate(stid = as.character(stids),
-         wrf_i = as.numeric(as.character(wrf_i)),
-         wrf_j = as.numeric(as.character(wrf_j)))
+euc.dist <- function(x1, x2){sqrt(sum((x1 - x2) ^ 2))}
+stid_wrf_ind <- data.frame(stid = select_stations$stid,
+                           wrf_i = 0, wrf_j = 0)
+pb <- progress_bar$new(total = dim(coords)[1]),
+                       format = " Calculating WRF indices [:bar] :percent")
+for(i in seq_along(coords[, 1])){
+  temp <- apply(wrf_coords, c(1, 2), euc.dist, coords[i, ])
+  ji <- which(temp == min(temp), arr.ind = TRUE)
+  stid_wrf_ind[i, 2:3] <- c(ji[2], ji[1])
+  pb$tick()
+}
 saveRDS(stid_wrf_ind, file.path(datadir, "stid_wrf_ind.Rds"))
 
 #------------------------------------------------------------------------------
