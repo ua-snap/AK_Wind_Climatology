7fd05e4edd2f4ec89206f55d2e9860a74c1927db
kmeanskyle <kmeanskyle@gmail.com>
2020-01-03 11:44:34 -0900

    quantile map all years of WRF downscaled ERA and GCM output

---
+++ WRF_code/qmap_era_all.R
@@ -0,0 +1,129 @@
+# quantile map ERA-Interim output, 
+#-- Setup ---------------------------------------------------------------------
+# remove spikes from adjusted ASOS data
+rm_spikes <- function(df, max_thr = 80) {
+  df <- filter(df, df$sped_adj < max_thr)
+  wsl <- list(ws = df$sped_adj, ts = df$t_round)
+  
+  # calculate speed and time differences 
+  calc_diffs <- function(olst) {
+    lapply(olst, diff)
+  }
+  # find indices of obs to remove
+  rm_obs <- function(dlst) {
+    rm_inc <- which(dlst$ws >= 35 & dlst$ts <= 2)
+    rm_dec <- which(dlst$ws <= -35 & dlst$ts <= 2)
+    union(rm_inc + 1, rm_dec)
+  }
+  
+  wsd <- calc_diffs(wsl)
+  rmv <- rm_obs(wsd)
+  if(length(rmv)) df[-rmv, ] else df
+}
+
+# qMap function to be mclapply'd to historical output
+wrap_qMap <- function(i) {
+  obs <- asos_adj[[i]] %>%
+    filter(t_round < hp)
+  sim <- era_adj[[i]] %>%
+    filter(ts < hp)
+  qmap_df <- qMap(obs$sped_adj, sim$sped)
+  sim$sped_adj <- qmap_df$sim_adj
+  sim
+}
+
+# create ecdfs for each station
+mk_ecdfs <- function(i) {
+  s1 <- sample(1:dim(asos_adj[[i]])[1], 5e4)
+  s2 <- sample(1:314064, 5e4)
+  stid <- era_adj[[i]]$stid[1]
+  p <- ggECDF_compare(
+    list(
+      obs = asos_adj[[i]]$sped_adj[s1],
+      sim = era_adj[[i]]$sped[s2],
+      sim_adj = era_adj[[i]]$sped_adj[s2]
+    ), 
+    p_title = paste0("ERA to ASOS Adj, ", stid),
+    xmin = 0
+  )
+  fn <- paste0(
+    "../AK_Wind_Climatology_aux/figures/qmap/ERA-Interim/", 
+    stid, ".png"
+  )
+  ggsave(fn, p, height = 4)
+}
+
+#------------------------------------------------------------------------------
+
+#-- Main ----------------------------------------------------------------------
+library(dplyr)
+library(lubridate)
+
+# end date for historical period
+hp <- ymd("2015-01-01")
+
+# use helper functions from Nome_Mets project
+source("../Nome_Mets/helpers.R")
+
+# gather ASOS data and adjust
+fns <- list.files(
+  "../AK_Wind_Climatology_aux/data/AK_ASOS_stations_adj", 
+  full.names = TRUE
+)
+
+cat("Reading and checking ASOS data files\n")
+te <- system.time({
+  asos_adj <- parallel::mclapply(fns, readRDS, mc.cores = 32)
+  # add station id's
+  names(asos_adj) <- unlist(
+    lapply(strsplit(fns, split = "/"), function(fv) {
+      gsub(".Rds", "", fv[length(fv)])
+    })
+  )
+})
+cat(paste0("ASOS data read, ", round(te[3], 1), " seconds\n"))
+te <- system.time(
+  asos_adj <- lapply(asos_adj, rm_spikes)
+)
+cat(paste0("ASOS data screened, ", round(te[3], 1), " seconds\n"))
+
+# gather ERA-Interim data that was already adjusted
+fns <- list.files(
+  "../AK_Wind_Climatology_aux/data/ERA_stations_adj", 
+  full.names = TRUE
+)
+
+cat("Reading ERA-Interim output 1980-2014\n")
+te <- system.time(
+  era_adj <- parallel::mclapply(fns, readRDS, mc.cores = 32)
+)
+cat(paste0("REA-Interim output read, ", round(te[3], 1), " seconds\n"))
+
+# stop if station order in lists doesn't match
+stopifnot(names(asos_adj) == unlist(lapply(era_adj, function(df) df$stid[1])))
+
+cat("quantile-mapping ERA-Interim 1980-2014\n")
+te <- system.time(
+  era_adj <- parallel::mclapply(
+    seq_along(era_adj), wrap_qMap, mc.cores = 32
+  )
+)
+cat(paste0("ERA-Interim output adjusted, ", round(te[3], 1), " seconds\n"))
+
+# add station id's
+names(era_adj) <- unlist(lapply(era_adj, function(df) {df$stid[1]}))
+
+cat("Saving adjusted ERA-Interim data\n")
+te <- system.time({
+  era_fn <- "../AK_Wind_Climatology_aux/data/era_adj.Rds"
+  saveRDS(era_adj, era_fn, compress = FALSE)
+})
+cat(paste0("ERA data saved, ", round(te[3], 1), " seconds\n"))
+
+cat("Saving ECDFs\n")
+te <- system.time(
+  parallel::mclapply(seq_along(era_adj), mk_ecdfs, mc.cores = 32)
+)
+cat(paste0("ECDFs saved, ", round(te[3], 1), " seconds\n"))
+
+#------------------------------------------------------------------------------
---
+++ WRF_code/qmap_wrf_all.R
@@ -0,0 +1,154 @@
+# quantile map all years of WRF output, future and historical
+#-- Setup ---------------------------------------------------------------------
+# qMap function to be mclapply'd to historical output
+wrap_qMap_his <- function(i, gcm = "CM3") {
+  obs <- era_adj[[i]] %>%
+    filter(ts < hp[2])
+  sim <- wrf_wdws[[paste0(gcm, "h")]][[i]] %>%
+    filter(ts >= hp[1])
+  qMap(obs$sped_adj, sim$ws, ret.deltas = TRUE)
+}
+
+# qMap function to be mclapply'd to future output
+wrap_qMap_fut <- function(i, gcm = "CM3") {
+  qMap(
+    sim = wrf_wdws[[paste0(gcm, "f")]][[i]]$ws,
+    use.deltas = wrf_adj[[paste0(gcm, "h")]][[i]]$deltas
+  )
+}
+
+# create ecdfs for each station
+mk_ecdfs <- function(i, gcm, pt) {
+  if(grepl("h", gcm)) {
+    obs_df <- era_adj[[i]]
+    sim_df <- wrf_adj[[gcm]][[i]]$df
+  } else {
+    obs_df <- wrf_adj[[gsub("f", "h", gcm)]][[i]]$df
+    sim_df <- wrf_adj[[gcm]][[i]]
+  }
+  s1 <- sample(1:dim(obs_df)[1], 5e4)
+  s2 <- sample(1:dim(sim_df)[1], 5e4)
+  stid <- era_adj[[i]]$stid[1]
+
+  p <- ggECDF_compare(
+    list(
+      obs = obs_df[s1, ncol(obs_df)],
+      sim = sim_df$sim[s2],
+      sim_adj = sim_df$sim_adj[s2]
+    ), 
+    p_title = paste0(pt, ", ", stid),
+    xmin = 0, var = "ws (mph)"
+  )
+  fn <- paste0(
+    "../AK_Wind_Climatology_aux/figures/qmap/", gcm, "/", stid, ".png"
+  )
+  ggsave(fn, p, height = 4)
+}
+
+#------------------------------------------------------------------------------
+
+#-- Main ----------------------------------------------------------------------
+suppressMessages({
+  library(dplyr)
+  library(lubridate)
+  library(ggplot2)
+  library(gridExtra)
+})
+
+source("../Nome_Mets/helpers.R")
+
+cat("Reading in WRF GCM and adjusted ERA-Interim output\n")
+te <- system.time({
+  wrf_wdws <- readRDS("../AK_Wind_Climatology_aux/data/wrf_wdws.Rds")
+  era_adj <- readRDS("../AK_Wind_Climatology_aux/data/era_adj.Rds")
+})
+cat(paste0("WRF output loaded, ", round(te[3], 1), " seconds\n"))
+
+# lists must be in same order
+stopifnot(names(wrf_wdws$CM3h) == names(era_adj))
+
+# start and end dates for historical period
+hp <- c(ymd("1980-01-01"), ymd("2006-01-01"))
+
+wrf_adj <- list()
+cat("quantile-mapping CM3 historical...\n")
+te <- system.time(
+  wrf_adj$CM3h <- parallel::mclapply(
+    seq_along(wrf_wdws$CM3h), wrap_qMap_his, mc.cores = 32
+  )
+)
+cat(paste0("CM3 historical output adjusted, ", round(te[3], 1), " seconds\n"))
+
+cat("quantile-mapping CCSM4 historical...\n")
+te <- system.time(
+  wrf_adj$CCSM4h <- parallel::mclapply(
+    seq_along(wrf_wdws$CCSM4h), wrap_qMap_his, gcm = "CCSM4", mc.cores = 32
+  )
+)
+cat(paste0("CCSM4 historical output adjusted, ", round(te[3], 1), " seconds\n"))
+
+# quantile map projected output
+cat("quantile-mapping CM3 future...\n")
+te <- system.time(
+  wrf_adj$CM3f <- parallel::mclapply(
+    seq_along(wrf_wdws$CM3f), wrap_qMap_fut, mc.cores = 32
+  )
+)
+cat(paste0("CM3 future output adjusted, ", round(te[3], 1), " seconds\n"))
+
+cat("quantile-mapping CCSM4 future...\n")
+te <- system.time(
+  wrf_adj$CCSM4f <- parallel::mclapply(
+    seq_along(wrf_wdws$CCSM4f), wrap_qMap_fut, gcm = "CCSM4", mc.cores = 32
+  )
+)
+cat(paste0("CCSM4 future output adjusted, ", round(te[3], 1), " seconds\n"))
+
+# save WRF otput
+cat("Saving adjusted WRF GCM data\n")
+te <- system.time({
+  wrf_fn <- "../AK_Wind_Climatology_aux/data/wrf_adj.Rds"
+  saveRDS(wrf_adj, wrf_fn, compress = FALSE)
+})
+cat(paste0(
+  "WRF GCM data saved as wrf_adj.Rds, ", round(te[3], 1), " seconds\n"
+))
+
+# generate and save ECDFs
+cat("Saving CM3 historical ECDFs\n")
+te <- system.time(
+  parallel::mclapply(
+    seq_along(wrf_wdws$CM3h), mk_ecdfs, 
+    gcm = "CM3h", pt = "CM3h to ERA adj", mc.cores = 32
+  )
+)
+cat(paste0("CM3 historical ECDFs saved, ", round(te[3], 1), " seconds\n"))
+
+cat("Saving CM3 future ECDFs\n")
+te <- system.time(
+  parallel::mclapply(
+    seq_along(wrf_wdws$CM3f), mk_ecdfs, 
+    gcm = "CM3f", pt = "CM3f to CM3h adj", mc.cores = 32
+  )
+)
+cat(paste0("CM3 Future ECDFs saved, ", round(te[3], 1), " seconds\n"))
+
+cat("Saving CCSM4 historical ECDFs\n")
+te <- system.time(
+  parallel::mclapply(
+    seq_along(wrf_wdws$CCSM4h), mk_ecdfs, 
+    gcm = "CCSM4h", pt = "CCSM4h to ERA adj", mc.cores = 32
+  )
+)
+cat(paste0("CCSM4 historical ECDFs saved, ", round(te[3], 1), " seconds\n"))
+
+cat("Saving CCSM4 future ECDFs\n")
+te <- system.time(
+  parallel::mclapply(
+    seq_along(wrf_wdws$CCSM4f), mk_ecdfs, 
+    gcm = "CCSM4f", pt = "CCSM4f to CCSM4h adj", mc.cores = 32
+  )
+)
+cat(paste0("CCSM4 future ECDFs saved, ", round(te[3], 1), " seconds\n"))
+
+#------------------------------------------------------------------------------
